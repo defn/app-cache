9ef601c3af1ff43472a72b4adad0db4be7ce301d
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-12 22:09:14 -0700

    boiletplate for cache

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "${var.provider_region}"
+}
+
+module "app" {
+  source = "../module-aws-app"
+
+  provider_region = "${var.provider_region}"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  az_count = "${var.az_count}"
+}
+
+module "default" {
+  source = "../module-aws-service"
+
+  provider_region = "${var.provider_region}"
+
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
+  az_count = "${var.az_count}"
+
+  cidr_blocks = "${var.cidr_blocks}"
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  count = "${var.az_count}"
+}
+
+resource "aws_nat_gateway" "nat" {
+  count = "${var.az_count}"
+
+  subnet_id = "${element(split(" ", module.nat.subnet_ids), count.index)}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+}
